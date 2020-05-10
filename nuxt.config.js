a98ec2c0b8c7968db82991b98b856f1c9018d7fb
Aleksandar Ratkov <alexandar.ratkov@gmail.com>
2020-05-10 21:26:05 +0200

    Add internalization

--- nuxt.config.js
+++ nuxt.config.js
@@ -30,6 +30,7 @@ export default {
   ** Plugins to load before mounting the App
   */
   plugins: [
+    "~/plugins/i18n.js"
   ],
   /*
   ** Nuxt.js dev-modules
@@ -52,13 +53,7 @@ export default {
       dark: true,
       themes: {
         dark: {
-          primary: colors.blue.darken2,
-          accent: colors.grey.darken3,
-          secondary: colors.amber.darken3,
-          info: colors.teal.lighten1,
-          warning: colors.amber.base,
-          error: colors.deepOrange.accent4,
-          success: colors.green.accent3
+          
         }
       }
     }
