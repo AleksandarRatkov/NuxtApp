fe937938c8d773cd2cc07d3d93428cf1dc19610d
Aleksandar Ratkov <alexandar.ratkov@gmail.com>
2020-05-10 21:34:15 +0200

    Add navbar and sidedrawer components

---
+++ components/Navbar.vue
@@ -0,0 +1,85 @@
+<template>
+<v-app-bar :clipped-left="$vuetify.breakpoint.lgAndUp" app src="/images/background.webp" dark>
+    <v-app-bar-nav-icon @click.native="setDrawer(!drawer)" />
+    <v-toolbar-title style="width: 300px" class="ml-0 pl-4">
+        <span class="hidden-sm-and-down">{{$t('navbar.welcome')}}</span>
+    </v-toolbar-title>
+    <v-text-field flat solo-inverted hide-details prepend-inner-icon="mdi-magnify" :label="$t('navbar.search')" class="hidden-sm-and-down" />
+    <v-spacer />
+    <v-badge bordered bottom color="green accent-4" dot offset-x="10" offset-y="10">
+        <v-avatar left size="40">
+            <!-- <v-img v-if="userProfile.profileImageUrl" :src="userProfile.profileImageUrl" alt="logoImage"></v-img>
+            <v-icon v-if="!userProfile.profileImageUrl" large>mdi-account-circle</v-icon> -->
+        </v-avatar>
+    </v-badge>
+
+    <v-tooltip bottom>
+        <template v-slot:activator="{ on }">
+            <!-- <v-btn right text v-on="on" @click="logout"> -->
+            <v-btn right text v-on="on">
+                <v-icon>mdi-logout</v-icon>
+            </v-btn>
+        </template>
+        <span>{{$t('navbar.logout')}}</span>
+    </v-tooltip>
+    <div class="locale">
+        <v-select v-model="$i18n.locale" :items="langs" item-value="locale" item-text="lang" outlined></v-select>
+    </div>
+</v-app-bar>
+</template>
+
+<script>
+import {
+    mapState,
+    // mapMutations,
+    // mapActions
+} from 'vuex'
+
+// import {
+//     auth
+// } from '~/plugins/firebaseConfig.js'
+
+export default {
+    name: 'Navbar',
+    data() {
+        return {
+            langs: [{
+                    locale: 'en',
+                    lang: 'En'
+                },
+                {
+                    locale: 'sr',
+                    lang: 'Sr'
+                }
+            ]
+        }
+    },
+    computed: {
+        ...mapState({
+            // userProfile: state => state.user.userProfile,
+            drawer: state => state.drawer
+        }),
+    },
+    // methods: {
+    //     ...mapMutations(['setDrawer']),
+    //     ...mapActions(['clearData']),
+    //     logout() {
+    //         auth.signOut()
+    //             .then(() => {
+    //                 this.clearData();
+    //                 this.$router.push("/login");
+    //             })
+    //             .catch(err => {
+    //                 console.log(err);
+    //             });
+    //     }
+    // },
+}
+</script>
+
+<style lang="scss" scoped>
+.locale {
+    width: 75px;
+    height: 58px;
+}
+</style>
---
+++ components/SideDrawer.vue
@@ -0,0 +1,54 @@
+<template>
+<v-navigation-drawer :value="drawer" :clipped="$vuetify.breakpoint.lgAndUp" app>
+    <v-list dense>
+        <template v-for="item in items">
+            <v-list-item :key="item.text" link>
+                <v-list-item-action>
+                    <v-icon>{{ item.icon }}</v-icon>
+                </v-list-item-action>
+                <v-list-item-content>
+                    <v-list-item-title>
+                        <nuxt-link tag="div" :to="item.route"> {{ $t(item.text) }} </nuxt-link>
+                    </v-list-item-title>
+                </v-list-item-content>
+            </v-list-item>
+        </template>
+    </v-list>
+</v-navigation-drawer>
+</template>
+
+<script>
+import {
+    mapState
+} from 'vuex'
+
+export default {
+    name: 'SideDrawer',
+    data() {
+        return {
+            items: [{
+                    icon: 'mdi-view-dashboard',
+                    text: 'sideDrawer.homepage',
+                    route: '/dashboard'
+                },
+                {
+                    icon: 'mdi-contacts',
+                    text: 'sideDrawer.contacts',
+                    route: '/contacts'
+                },
+                {
+                    icon: 'mdi-account-cog',
+                    text: 'sideDrawer.settings',
+                    route: '/settings'
+                }
+            ]
+        }
+    },
+    computed: {
+        ...mapState(['drawer'])
+    },
+}
+</script>
+
+<style lang="scss" scoped>
+</style>
