70abd1a036aa08da39474eaccf5055c7880a519b
Aleksandar Ratkov <alexandar.ratkov@gmail.com>
2020-05-10 21:25:20 +0200

    Add store

---
+++ store/index.js
@@ -0,0 +1,18 @@
+export const state = () => ({
+    drawer: true,
+})
+
+export const mutations = {
+    setDrawer(state, value) {
+        state.drawer = value;
+    },
+}
+
+
+export const actions = {
+    // clearData({ commit }) {
+    //     commit('user/setCurrentUser', null)
+    //     commit('user/setUserProfile', {})
+    //     commit('post/setPosts', [])
+    // }
+}
